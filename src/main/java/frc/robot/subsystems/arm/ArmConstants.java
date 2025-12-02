9f3f448a5f8e0ed89ce91433cd028ed353436a20
Jaely <10117985@philasd.org>
2025-12-01 19:58:01 -0500

    Recoded arm and created commands for arm

---
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.units.measure.Angle;
+import static edu.wpi.first.units.Units.Degrees;
+
+
+public class ArmConstants {
+   
+public static final double kSmartCurrentLimit = 60;
+public static final double kVoltageCompensation = 0;
+public static final boolean kInverted = false;
+public static final boolean kBrake = false;
+public static final Angle kLevelFourAngle = Degrees.of(0);
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/arm/armcommands/ArmTuning.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.arm.armcommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ArmTuning extends Command {
+    
+ 
+    public ArmTuning(){
+        
+    }
+
+    public void execute(){
+       
+    }
+}
