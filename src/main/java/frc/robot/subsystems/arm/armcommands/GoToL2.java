ed03a0c77c008bcbd693f04f667bf93544dcbb4b
Jaely <10117985@philasd.org>
2025-12-03 17:09:36 -0500

    Added L2 and L3 commands

--- src/main/java/frc/robot/subsystems/arm/armcommands/GoToL2.java
+++ src/main/java/frc/robot/subsystems/arm/armcommands/GoToL2.java
@@ -1,29 +1,24 @@
 package frc.robot.subsystems.arm.armcommands;
 
-import com.revrobotics.RelativeEncoder;
+import java.util.concurrent.RejectedExecutionHandler;
 
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmConstants;
 
-
-public class GoToL4 extends Command {
-
+public class GoToL2 extends Command {
+    
     private Arm armMotor;
 
-    public GoToL4(Arm armMotor){
+    public GoToL2(Arm armMotor) {
         this.armMotor = armMotor;
     }
-    
-    public void execute(){
-        armMotor.goToL4();
 
+    public void execute(){
+        armMotor.goToL2();
     }
 
     public boolean isFinished(){
-        return armMotor.getAngle() == ArmConstants.kLevelFourAngle;
+        return armMotor.getAngle() == ArmConstants.kLevelTwoAngle;
     }
-
-  
 }
--- src/main/java/frc/robot/subsystems/arm/armcommands/GoToL3.java
+++ src/main/java/frc/robot/subsystems/arm/armcommands/GoToL3.java
@@ -1,29 +1,24 @@
 package frc.robot.subsystems.arm.armcommands;
 
-import com.revrobotics.RelativeEncoder;
-
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmConstants;
 
 
-public class GoToL4 extends Command {
+public class GoToL3 extends Command {
 
     private Arm armMotor;
-
-    public GoToL4(Arm armMotor){
+    
+    public GoToL3(Arm armMotor){
         this.armMotor = armMotor;
     }
-    
-    public void execute(){
-        armMotor.goToL4();
 
+    public void execute(){
+        armMotor.goToL3();
     }
 
     public boolean isFinished(){
-        return armMotor.getAngle() == ArmConstants.kLevelFourAngle;
+        return armMotor.getAngle() == ArmConstants.kLevelThreeAngle;
     }
-
-  
-}
+ }
